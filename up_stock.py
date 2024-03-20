f6932d6eb6d6cade3d32fa40b1140ff91e41c7a4
dava0086 <dava0086@gmail.com>
2024-03-20 20:51:50 +0800

    涨停股票预测脚本。

--- up_stock.py
+++ up_stock.py
@@ -14,12 +14,13 @@ def is_possible(result: list[Card]):
     for c in result:
         if c.get_direct():
             cnt += 1
-    return cnt > len(result) // 2
+    # return cnt > len(result) // 2
+    return cnt == len(result)
 
 
 def req_digit(cs: CardStack, idx: str):
     for i in range(10):
-        an = answer_me(cs, f'它的第{idx}位是{i}吗？')
+        an = answer_me(cs, f'它的代码第{idx}位是{i}吗？')
         if is_possible(an):
             return i
 
@@ -27,22 +28,22 @@ def req_digit(cs: CardStack, idx: str):
 def req_shanghai(cs: CardStack):
     a1 = answer_me(cs, '它的代码第二位是8吗？')
     if is_possible(a1):
-        print('88', end='')
+        print('88', end='', flush=True)
         for i in ('四', '五', '六'):
             d = req_digit(cs, i)
             if d is not None:
-                print(d, end='')
+                print(d, end='', flush=True)
                 continue
-            print('..\t搞错了再来！')
+            print('..\t搞错了再来！', flush=True)
             return False
     else:
-        print('0', end='')
+        print('0', end='', flush=True)
         for i in ('三', '四', '五', '六'):
             d = req_digit(cs, i)
             if d is not None:
-                print(d, end='')
+                print(d, end='', flush=True)
                 continue
-            print('..\t搞错了再来！')
+            print('..\t搞错了再来！', flush=True)
             return False
     return True
 
@@ -50,22 +51,22 @@ def req_shanghai(cs: CardStack):
 def req_shenzhen(cs: CardStack):
     a1 = answer_me(cs, '它的代码第一位是3吗？')
     if is_possible(a1):
-        print('30', end='')
+        print('30', end='', flush=True)
         for i in ('三', '四', '五', '六'):
             d = req_digit(cs, i)
             if d is not None:
-                print(d, end='')
+                print(d, end='', flush=True)
                 continue
-            print('..\t搞错了再来！')
+            print('..\t搞错了再来！', flush=True)
             return False
     else:
-        print('00', end='')
+        print('00', end='', flush=True)
         for i in ('三', '四', '五', '六'):
             d = req_digit(cs, i)
             if d is not None:
-                print(d, end='')
+                print(d, end='', flush=True)
                 continue
-            print('..\t搞错了再来！')
+            print('..\t搞错了再来！', flush=True)
             return False
     return True
 
@@ -73,12 +74,15 @@ def req_shenzhen(cs: CardStack):
 def calc(cs: CardStack):
     a1 = answer_me(cs, '明天会涨停的股票在上交所吗？')
     if is_possible(a1):
-        print('6', end='')
+        print('6', end='', flush=True)
         if req_shanghai(cs):
             print()
             return
         return calc(cs)
     else:
+        a1 = answer_me(cs, '明天会涨停的股票在深交所吗？')
+        if not is_possible(a1):
+            return calc(cs)
         if req_shenzhen(cs):
             print()
             return
